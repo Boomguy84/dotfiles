5e928452df142022bd7b1177684296ae769f5886
scott <smurr4@uwo.ca>
2025-10-08 21:27:49 -0400

    fixed the wallpaper themeing script so that when waybar is ran from hyprland.conf it knows where stuff is

--- .config/scripts/wallpaper_theming.sh
+++ .config/scripts/wallpaper_theming.sh
@@ -2,6 +2,12 @@
 set -euo pipefail
 
 # --- SETTINGS ---------------------------------------------------------------
+
+# Make sure the system knows where shit is
+export PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-unix:path=${XDG_RUNTIME_DIR}/bus}"
+
 # Folder containing wallpapers (change if you keep them elsewhere)
 WALL_DIR="$HOME/.config/wallpapers"
 
